b3c1eeb11fade458600c7e2ad659dd6da6162188
Jobba7 <johannes.pforr@outlook.de>
2025-03-30 18:03:02 +0200

    Enable allow_unsafe_werkzeug option in socketio.run for development purposes

--- server.py
+++ server.py
@@ -234,4 +234,4 @@ if __name__ == "__main__":
     sensor_thread.start()
 
     # Start the Flask app with WebSockets
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
